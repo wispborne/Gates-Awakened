e85554c48e80740ca2d4dba40be9bfdb050e7f44
Ken Chiang <ken.chiang@bigswitch.com>
2017-07-09 21:30:36 -0700

    Implement gate activation.

--- jars/src/FlyThroughGateCommandPlugin.java
+++ jars/src/FlyThroughGateCommandPlugin.java
@@ -4,18 +4,16 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
+
+import com.fs.starfarer.api.campaign.*;
 import org.lwjgl.util.vector.Vector2f;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.util.Misc;
-import com.fs.starfarer.api.campaign.InteractionDialogAPI;
-import com.fs.starfarer.api.campaign.CampaignFleetAPI;
-import com.fs.starfarer.api.campaign.LocationAPI;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
-import com.fs.starfarer.api.campaign.SectorEntityToken;
 import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
 import com.fs.starfarer.api.impl.campaign.ids.Tags;
 
-public class ActiveGatesCommandPlugin extends BaseCommandPlugin {
+public class FlyThroughGateCommandPlugin extends data.campaign.rulecmd.GateCommandPlugin {
 
     @Override
     public boolean execute(String ruleId, InteractionDialogAPI dialog,
@@ -24,26 +22,44 @@ public class ActiveGatesCommandPlugin extends BaseCommandPlugin {
 
         if (dialog == null) return false;
 
-        List<LocationAPI> systemsWithGates = new ArrayList<>();
+        TextPanelAPI textPanel = dialog.getTextPanel();
+
+        // can only fly through activated gates
+        if (!dialog.getInteractionTarget().hasTag(ACTIVATED)) {
+            textPanel.addParagraph("The gate is not activated.");
+            textPanel.addParagraph("Nothing happens.");
+            return false;
+        }
+
+        // FIXME pay fuel cost to use gate?
+
+        List<LocationAPI> systemsWithActivatedGates = new ArrayList<>();
         Random rng = new Random();
         for (LocationAPI system : Global.getSector().getStarSystems())
         {
             List<SectorEntityToken> candidates =
-                system.getEntitiesWithTag(Tags.GATE);
+                system.getEntitiesWithTag(ACTIVATED);
             if (candidates.size() > 0)
             {
-                systemsWithGates.add(system);
+                systemsWithActivatedGates.add(system);
             }
         }
-        LocationAPI newSys = systemsWithGates.get(rng.nextInt(systemsWithGates.size()));
+        LocationAPI newSys = systemsWithActivatedGates.get(
+                rng.nextInt(systemsWithActivatedGates.size()));
         CampaignFleetAPI playerFleet = Global.getSector().getPlayerFleet();
-        playerFleet.getContainingLocation().removeEntity(playerFleet);
+        LocationAPI oldSys = playerFleet.getContainingLocation();
+        oldSys.removeEntity(playerFleet);
         newSys.addEntity(playerFleet);
         Global.getSector().setCurrentLocation(newSys);
         List<SectorEntityToken> gates = newSys.getEntitiesWithTag(Tags.GATE);
         Vector2f newVect = gates.get(0).getLocation();
         playerFleet.setLocation(newVect.x, newVect.y);
         playerFleet.clearAssignments();
+
+        textPanel.addParagraph("Your fleet passes through the gate...");
+        if (oldSys == newSys) {
+            textPanel.addParagraph("and nothing happens.");
+        }
 
         return true;
     }
---
+++ jars/src/GateCommandPlugin.java
@@ -0,0 +1,7 @@
+package data.campaign.rulecmd;
+
+import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
+
+public abstract class GateCommandPlugin extends BaseCommandPlugin {
+    public static final String ACTIVATED = "gate_activated";
+}
