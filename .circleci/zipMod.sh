88fc784eb8cf719ea718e0277a9a0dbebc4db8c2
David Whitman <whitmadm@gmail.com>
2019-12-26 19:38:47 -0500

    ci

--- .circleci/zipMod.sh
+++ .circleci/zipMod.sh
@@ -6,5 +6,5 @@ modFolderName="Gates-Awakened"
 version=$(git describe --tags)
 zipName=$modFolderName-$version.zip
 git archive master -o $zipName --prefix $modFolderName-$version/
-mkdir artifacts
-mv $zipName artifacts/
+mkdir -p ./artifacts
+mv ./$zipName ./artifacts/
